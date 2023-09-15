4ec70dea312753effa46d46ee68c6af1c4d0236a
Jordan Rodriguez <miguel20010807@gmail.com>
2023-09-15 09:54:29 -0600

    BD

--- Assets/cultech (3).sql
+++ Assets/cultech (3).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 11-09-2023 a las 08:16:26
+-- Tiempo de generación: 15-09-2023 a las 17:52:28
 -- Versión del servidor: 10.4.24-MariaDB
 -- Versión de PHP: 8.1.6
 
@@ -46,11 +46,12 @@ CREATE TABLE `configuracion` (
   `id_cultivo` int(10) NOT NULL,
   `tem_max` decimal(10,2) NOT NULL,
   `tem_min` decimal(10,2) NOT NULL,
-  `humendad_max` decimal(10,2) NOT NULL,
-  `humendad_min` decimal(10,2) NOT NULL,
+  `humedad_max` decimal(10,2) NOT NULL,
+  `humedad_min` decimal(10,2) NOT NULL,
   `stem_max` decimal(10,2) NOT NULL,
-  `shumendad_max` decimal(10,2) NOT NULL,
-  `shumendad_min` decimal(10,2) NOT NULL,
+  `stem_min` decimal(10,2) NOT NULL,
+  `shumedad_max` decimal(10,2) NOT NULL,
+  `shumedad_min` decimal(10,2) NOT NULL,
   `altura` decimal(10,2) NOT NULL,
   `dias` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
@@ -64,7 +65,6 @@ CREATE TABLE `configuracion` (
 CREATE TABLE `cultivos` (
   `id` int(10) NOT NULL,
   `id_placa` int(10) NOT NULL,
-  `id_configuracion` int(10) NOT NULL,
   `nombre` varchar(50) NOT NULL,
   `fecha` date NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
@@ -137,17 +137,26 @@ CREATE TABLE `plantillas` (
   `nombre` varchar(50) NOT NULL,
   `tem_max` decimal(10,2) NOT NULL,
   `tem_min` decimal(10,2) NOT NULL,
-  `humendad_max` decimal(10,2) NOT NULL,
-  `humendad_min` decimal(10,2) NOT NULL,
+  `humedad_max` decimal(10,2) NOT NULL,
+  `humedad_min` decimal(10,2) NOT NULL,
   `stem_max` decimal(10,2) NOT NULL,
   `stem_min` decimal(10,2) NOT NULL,
-  `shumendad_max` decimal(10,2) NOT NULL,
-  `shumendad_min` decimal(10,2) NOT NULL,
+  `shumedad_max` decimal(10,2) NOT NULL,
+  `shumedad_min` decimal(10,2) NOT NULL,
   `altura` decimal(10,2) NOT NULL,
   `dias` int(11) NOT NULL,
   `foto` varchar(50) NOT NULL,
   `estado` int(1) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `plantillas`
+--
+
+INSERT INTO `plantillas` (`id`, `id_usuario`, `nombre`, `tem_max`, `tem_min`, `humedad_max`, `humedad_min`, `stem_max`, `stem_min`, `shumedad_max`, `shumedad_min`, `altura`, `dias`, `foto`, `estado`) VALUES
+(1, 1, 'Pepinos', '10.00', '9.00', '8.00', '7.00', '6.00', '5.00', '4.00', '3.00', '2.00', 10, '1.png', 0),
+(2, 1, 'Chiles Verdes', '1.00', '2.00', '3.00', '4.00', '5.00', '6.00', '7.00', '8.00', '9.00', 10, '2.png', 0),
+(3, 1, 'Tomates', '5.00', '5.00', '5.00', '5.00', '5.00', '5.00', '5.00', '5.00', '5.00', 5, '3.png', 0);
 
 -- --------------------------------------------------------
 
@@ -169,7 +178,8 @@ CREATE TABLE `usuarios` (
 --
 
 INSERT INTO `usuarios` (`id`, `nombre`, `apellido`, `correo`, `clave`, `perfil`) VALUES
-(1, 'Miguel Jordán', 'Rodríguez Reyes', 'mrodriguez74@ucol.mx', 'ef797c8118f02dfb649607dd5d3f8c7623048c9c063d532cc95c5ed7a898a64f', 'undraw_profile.svg');
+(1, 'Miguel Jordán', 'Rodríguez Reyes', 'mrodriguez74@ucol.mx', 'ef797c8118f02dfb649607dd5d3f8c7623048c9c063d532cc95c5ed7a898a64f', 'undraw_profile.svg'),
+(4, 'Usuario', 'Prueba', 'admin@ucol.mx', 'ef797c8118f02dfb649607dd5d3f8c7623048c9c063d532cc95c5ed7a898a64f', 'undraw_profile.svg');
 
 --
 -- Índices para tablas volcadas
@@ -179,43 +189,52 @@ INSERT INTO `usuarios` (`id`, `nombre`, `apellido`, `correo`, `clave`, `perfil`)
 -- Indices de la tabla `acciones`
 --
 ALTER TABLE `acciones`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_placa` (`id_placa`);
 
 --
 -- Indices de la tabla `configuracion`
 --
 ALTER TABLE `configuracion`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_cultivo` (`id_cultivo`);
 
 --
 -- Indices de la tabla `cultivos`
 --
 ALTER TABLE `cultivos`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_placa` (`id_placa`);
 
 --
 -- Indices de la tabla `monitoreo`
 --
 ALTER TABLE `monitoreo`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_placa` (`id_placa`);
 
 --
 -- Indices de la tabla `notificaciones`
 --
 ALTER TABLE `notificaciones`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_usuario` (`id_usuario`);
 
 --
 -- Indices de la tabla `placas`
 --
 ALTER TABLE `placas`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_usuario` (`id_usuario`),
+  ADD KEY `id_usuario_2` (`id_usuario`),
+  ADD KEY `id_placa` (`id_placa`);
 
 --
 -- Indices de la tabla `plantillas`
 --
 ALTER TABLE `plantillas`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_usuario` (`id_usuario`);
 
 --
 -- Indices de la tabla `usuarios`
@@ -267,13 +286,47 @@ ALTER TABLE `placas`
 -- AUTO_INCREMENT de la tabla `plantillas`
 --
 ALTER TABLE `plantillas`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `configuracion`
+--
+ALTER TABLE `configuracion`
+  ADD CONSTRAINT `configuracion_ibfk_1` FOREIGN KEY (`id_cultivo`) REFERENCES `cultivos` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `cultivos`
+--
+ALTER TABLE `cultivos`
+  ADD CONSTRAINT `cultivos_ibfk_1` FOREIGN KEY (`id_placa`) REFERENCES `placas` (`id_placa`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `notificaciones`
+--
+ALTER TABLE `notificaciones`
+  ADD CONSTRAINT `notificaciones_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `placas`
+--
+ALTER TABLE `placas`
+  ADD CONSTRAINT `placas_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `plantillas`
+--
+ALTER TABLE `plantillas`
+  ADD CONSTRAINT `plantillas_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
