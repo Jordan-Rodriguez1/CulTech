facf291365af1a56b3f9a8feaac9ac608cb26144
Jordan Rodriguez <miguel20010807@gmail.com>
2023-10-05 11:13:30 -0600

    Bug

--- Config/Config.php
+++ Config/Config.php
@@ -1,5 +1,6 @@
 <?php
-    const BASE_URL = "http://localhost/CulTech/";
+    //const BASE_URL = "http://localhost/CulTech/";
+    const BASE_URL = "http://192.168.1.5/CulTech/";
     const HOST = "localhost";
     const BD = "cultech";
     const DB_USER = "root";
